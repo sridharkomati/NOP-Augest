3333b303a87b88ad7a026cab83c4b50cfc38c832
sridhar <sridhar98855@gmail.com>
2023-08-30 23:12:16 +0530

    added EC2

--- deploy/provider.tf
+++ deploy/provider.tf
@@ -1,4 +1,12 @@
+terraform {
+  required_version = ">=1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=4.47.0"
+    }
+  }
+}
 provider "aws" {
-  region = "us-east-1"
-
+  region = var.region
 }
