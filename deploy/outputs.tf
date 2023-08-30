aca460c6a40ce13783fe6d098ab05c3e1b8ecfc7
sridhar <sridhar98855@gmail.com>
2023-08-30 23:17:26 +0530

    added EC2

---
+++ deploy/outputs.tf
@@ -0,0 +1,17 @@
+output "vpc_id" {
+
+  value =aws_vpc.ntier.id
+}
+
+
+output "vpc_cidr_block" {
+
+  value = var.ntier_vpc_info.vpc_cidr
+}
+
+output "subnet_ids" {
+
+  value = compact(aws_subnet.subnets[*].cidr_block)
+}
+
+
